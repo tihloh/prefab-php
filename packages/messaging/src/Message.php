cb910ae62e229482c16c31a7980ebcca035d0a37
Christian Bustamante <tihloh12@gmail.com>
2026-08-25 11:37:51 +0800

    Expand message envelope and attachments

--- packages/messaging/src/Message.php
+++ packages/messaging/src/Message.php
@@ -6,16 +6,30 @@ namespace Tihloh\Prefab\Messaging;
 
 final class Message
 {
-    /** @param array<string, mixed> $metadata */
+    /**
+     * @param list<string> $cc
+     * @param list<string> $bcc
+     * @param list<Attachment> $attachments
+     * @param array<string, mixed> $metadata
+     */
     public function __construct(
         public readonly string $subject = '',
         public readonly string $text = '',
         public readonly ?string $html = null,
         public readonly array $metadata = [],
+        public readonly array $cc = [],
+        public readonly array $bcc = [],
+        public readonly ?string $replyTo = null,
+        public readonly array $attachments = [],
     ) {}
 
     public static function make(string $text = '', string $subject = ''): self
     {
         return new self($subject, $text);
+    }
+
+    public function withAttachment(Attachment $attachment): self
+    {
+        return new self($this->subject, $this->text, $this->html, $this->metadata, $this->cc, $this->bcc, $this->replyTo, [...$this->attachments, $attachment]);
     }
 }
