de4108ec8611c331a81bc23d6299023a7114655d
Christian Bustamante <tihloh12@gmail.com>
2026-08-29 15:37:47 +0800

    Trace permission checks

--- packages/permissions/src/DTOs/PermissionResult.php
+++ packages/permissions/src/DTOs/PermissionResult.php
@@ -2,6 +2,8 @@
 
 namespace Tihloh\Prefab\Permissions\DTOs;
 
+use Tihloh\Prefab\PrefabRuntime;
+
 final class PermissionResult
 {
     public function __construct(
@@ -9,5 +11,13 @@ final class PermissionResult
         public string $source,
         public array $groups = [],
         public array $deniedGroups = [],
-    ) {}
+    ) {
+        PrefabRuntime::traceStart('permissions', 'check');
+        PrefabRuntime::traceEnd([
+            'allowed' => $allowed,
+            'source' => $source,
+            'groups' => count($groups),
+            'denied_groups' => count($deniedGroups),
+        ]);
+    }
 }
