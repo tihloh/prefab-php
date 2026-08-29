c4741d1e8f124ea3104a1e3a27d87690a68078b8
Christian Bustamante <tihloh12@gmail.com>
2026-08-29 15:37:05 +0800

    Trace messaging delivery results

--- packages/messaging/src/DeliveryResult.php
+++ packages/messaging/src/DeliveryResult.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Tihloh\Prefab\Messaging;
 
+use Tihloh\Prefab\PrefabRuntime;
+
 final class DeliveryResult
 {
     /** @param array<string, mixed> $metadata */
@@ -13,7 +15,16 @@ final class DeliveryResult
         public readonly ?string $messageId = null,
         public readonly ?string $error = null,
         public readonly array $metadata = [],
-    ) {}
+    ) {
+        PrefabRuntime::traceStart('messaging', 'send', [
+            'channel' => $channel,
+        ]);
+        PrefabRuntime::traceEnd([
+            'successful' => $successful,
+            'message_id' => $messageId,
+            'error' => $error,
+        ]);
+    }
 
     public function failed(): bool
     {
