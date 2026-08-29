d2b6b16fd8fa32a878feae19cbc1874718cb0179
Christian Bustamante <tihloh12@gmail.com>
2026-08-29 15:37:22 +0800

    Trace authentication results

--- packages/auth/src/DTOs/AuthResult.php
+++ packages/auth/src/DTOs/AuthResult.php
@@ -3,6 +3,7 @@
 namespace Tihloh\Prefab\Auth\DTOs;
 
 use Tihloh\Prefab\Auth\Contracts\AuthenticatableUserInterface;
+use Tihloh\Prefab\PrefabRuntime;
 
 final class AuthResult
 {
@@ -11,5 +12,13 @@ final class AuthResult
         public ?AuthenticatableUserInterface $user = null,
         public ?array $log = null,
         public ?string $reason = null,
-    ) {}
+    ) {
+        $operation = (string) ($log['action'] ?? 'attempt');
+        PrefabRuntime::traceStart('auth', $operation);
+        PrefabRuntime::traceEnd([
+            'success' => $success,
+            'user_id' => $user?->authIdentifier(),
+            'reason' => $reason,
+        ]);
+    }
 }
