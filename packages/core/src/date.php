649f0c06d8e80ce77979f5019e7144d4042a25fe
Christian Bustamante <tihloh12@gmail.com>
2026-08-31 21:11:11 +0800

    Simplify date time helpers

--- packages/core/src/date.php
+++ packages/core/src/date.php
@@ -2,15 +2,25 @@
 
 declare(strict_types=1);
 
-use DateTimeZone;
 use Tihloh\Prefab\Core\DateTime\Date;
 
-if (!function_exists('prefab_date')) {
-    function prefab_date(
-        mixed $value = null,
-        ?string $format = null,
-        DateTimeZone|string|null $timezone = null
-    ): Date {
-        return Date::make($value, $format, $timezone);
+if (!function_exists('now')) {
+    function now(): Date
+    {
+        return Date::make();
+    }
+}
+
+if (!function_exists('datetime')) {
+    function datetime(mixed $value = null): Date
+    {
+        return Date::make($value);
+    }
+}
+
+if (!function_exists('prefab_datetime')) {
+    function prefab_datetime(mixed $value = null): Date
+    {
+        return Date::make($value);
     }
 }
